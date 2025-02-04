7c61b361ca79db707d6a7423e55af01c3bda5808
Eduard Boytsov <eduard.boytsov@gmail.com>
2025-02-04 18:38:40 -0500

    adjusted twosum
    adjusted solution to run in VSCode

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,2 +1,66 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Two Sum
+taken from Leetcode (https://leetcode.com/problems/two-sum/description/)
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order.
+
+Example 1:
+
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+Example 2:
+
+Input: nums = [3,2,4], target = 6
+Output: [1,2]
+Example 3:
+
+Input: nums = [3,3], target = 6
+Output: [0,1]
+ 
+
+Constraints:
+
+2 <= nums.length <= 104
+-109 <= nums[i] <= 109
+-109 <= target <= 109
+Only one valid answer exists.
+ 
+
+Follow-up: Can you come up with an algorithm that is less than O(n2) time complexity?
+*/
+using System;
+using System.Collections.Generic;
+
+public class Program { //public class Solution on the actual website
+    public static void Main(string[] args) {
+        int[] nums1 = {2, 7, 11, 15};
+        int target1 = 9;
+        Console.WriteLine(string.Join(", ", TwoSum(nums1, target1))); // Expected Output: 0, 1
+
+        int[] nums2 = {3, 2, 4};
+        int target2 = 6;
+        Console.WriteLine(string.Join(", ", TwoSum(nums2, target2))); // Expected Output: 1, 2
+
+        int[] nums3 = {3, 3};
+        int target3 = 6;
+        Console.WriteLine(string.Join(", ", TwoSum(nums3, target3))); // Expected Output: 0, 1
+    }
+
+    public static int[] TwoSum(int[] nums, int target) {
+        var dic = new Dictionary<int, int>();
+
+        for (int i = 0; i < nums.Length; i++) {
+            int complement = target - nums[i]; // The value we are looking for
+            if (dic.ContainsKey(complement)) {
+                return new int[] { dic[complement], i };
+            }
+            dic[nums[i]] = i; // Store the index of the current number
+        }
+
+        throw new Exception("No valid pair found"); // If no solution is found, throw an error
+    }
+}
+
--- ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc811b5e768a569a3356233616cffe71e00324a0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6218da26a3134c777dad27b02518a54be5d8ead3")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
